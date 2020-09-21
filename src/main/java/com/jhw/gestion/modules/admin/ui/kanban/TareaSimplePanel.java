ef647be353a435c3b51b1b0736255a5b0f36a04f
Unknown <unknown@example.com>
2020-09-20 22:23:25 -0500

    TareaSimplePanel: +buttonEdit, +panelPrioridad

--- src/main/java/com/jhw/gestion/modules/admin/ui/kanban/TareaSimplePanel.java
+++ src/main/java/com/jhw/gestion/modules/admin/ui/kanban/TareaSimplePanel.java
@@ -6,14 +6,21 @@
 package com.jhw.gestion.modules.admin.ui.kanban;
 
 import com.jhw.gestion.modules.admin.core.domain.TareaDomain;
+import com.jhw.personalization.core.domain.Personalization;
+import com.jhw.personalization.services.PersonalizationHandler;
+import com.jhw.swing.material.components.button.MaterialButtonIcon;
+import com.jhw.swing.material.components.button.MaterialButtonsFactory;
 import com.jhw.swing.material.components.button._MaterialButton;
+import com.jhw.swing.material.components.container.MaterialContainersFactory;
 import com.jhw.swing.material.components.container.panel._MaterialPanel;
 import com.jhw.swing.material.components.labels.MaterialLabel;
 import com.jhw.swing.material.components.labels.MaterialLabelsFactory;
 import com.jhw.swing.material.injection.MaterialSwingInjector;
 import com.jhw.utils.interfaces.Update;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import javax.swing.BorderFactory;
+import javax.swing.JPanel;
 import javax.swing.border.Border;
 
 /**
@@ -29,8 +36,8 @@ public class TareaSimplePanel extends _MaterialPanel implements Update {
     private final TareaDomain tarea;
 
     public TareaSimplePanel(TareaDomain tarea) {
-        initComponents();
         this.tarea = tarea;
+        initComponents();
         update();
     }
 
@@ -40,8 +47,24 @@ public class TareaSimplePanel extends _MaterialPanel implements Update {
 
         header = MaterialLabelsFactory.build();
         this.add(header, BorderLayout.NORTH);
+
+        buttonEdit = MaterialButtonsFactory.buildIconTransparent();
+        buttonEdit.setRippleColor(PersonalizationHandler.getColor(Personalization.KEY_COLOR_BUTTON_EDIT));
+        buttonEdit.setIcon(PersonalizationHandler.getDerivableIcon(Personalization.KEY_ICON_BUTTON_EDIT));
+
+        panelPrioridad = PrioridadSimplePanel.from(tarea.getPrioridadFk());
+
+        JPanel down = MaterialContainersFactory.buildPanelTransparent();
+        down.setLayout(new BorderLayout());
+
+        down.add(buttonEdit, BorderLayout.WEST);
+        down.add(panelPrioridad, BorderLayout.EAST);
+        this.add(down, BorderLayout.SOUTH);
     }
+
     private MaterialLabel header;
+    private MaterialButtonIcon buttonEdit;
+    private PrioridadSimplePanel panelPrioridad;
 
     @Override
     public void update() {
