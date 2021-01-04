54d62b1d4adaa1b91bd6d5be1f994b339b2e11a6
Unknown <unknown@example.com>
2021-01-04 15:36:59 -0500

    Rename A_ModuleAdminKanbanRESTConfig

--- src/main/java/com/root101/module/admin/kanban/rest/A_ModuleAdminKanbanRESTConfig.java
+++ src/main/java/com/root101/module/admin/kanban/rest/A_ModuleAdminKanbanRESTConfig.java
@@ -18,6 +18,7 @@ package com.root101.module.admin.kanban.rest;
 
 import com.root101.module.admin.kanban.core.module.KanbanCoreModule;
 import com.root101.module.admin.kanban.core.usecase_def.*;
+import com.root101.module.admin.kanban.repo.module.KanbanRepoModule;
 import com.root101.module.admin.kanban.service.ResourceServiceServerImplementation;
 import org.springframework.stereotype.Component;
 import com.root101.module.admin.kanban.service.ResourceServiceImplementation;
@@ -28,9 +29,9 @@ import com.root101.module.admin.kanban.service.ResourceServiceImplementation;
  * @author JesusHdezWaterloo@Github
  */
 @Component
-public class A_ModuleAdminKanban {
+public class A_ModuleAdminKanbanRESTConfig {
 
-    public static final String BASE_PACKAGE = "com.root101.module.admin.kanban";
+    public static final String BASE_PACKAGE = "com.root101.module.admin.kanban.rest";
 
     public final static ColumnaUseCase columnsUC;
     public final static PrioridadUseCase prioridadUC;
@@ -41,7 +42,7 @@ public class A_ModuleAdminKanban {
         ResourceServiceImplementation.init();//creo el resource general
         ResourceServiceServerImplementation.init();//creo el resource general
 
-        KanbanCoreModule.init();
+        KanbanCoreModule.init(KanbanRepoModule.init());
 
         columnsUC = KanbanCoreModule.getInstance().getImplementation(ColumnaUseCase.class);
         prioridadUC = KanbanCoreModule.getInstance().getImplementation(PrioridadUseCase.class);
